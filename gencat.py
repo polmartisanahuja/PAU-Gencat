f3bd25e03c62d73b77830e5f85a6d3185079d1cc
polstein <pol.marti.sanahuja@gmail.com>
2012-08-10 02:05:17 +0200

    Noisely magnitudes are now enabled

--- gencat.py
+++ gencat.py
@@ -4,6 +4,19 @@ import scipy as sp
 import scipy.interpolate
 from parameters import *
 
+#Functions.............................
+def err_magnitude():
+
+	#Computing photons number
+	N_sig = (3631.0 * 1.51 * pow(10, 7) * pow(10,-0.4 * m) * in_r[i] * t_exp[i] * tel_surface) / n_pix #object photons per pixel
+	N_sky = in_sky[i] * t_exp[i] * tel_surface * pix_size #sky photons per pixel
+
+	#Computing Signal to Noise (StoN)
+	StoN = (sqrt(n_pix * n_exp) * N_sig) / sqrt(N_sig + N_sky + RN * RN)
+	err_m_obs = 2.5 * log10(1 + 1 / StoN)
+
+	return err_m_obs 
+
 #Loading input catalog file.............
 cat = np.loadtxt(incat_file + incat_file_extention, usecols = (index_id, index_m, index_z, index_t), unpack = 'True')
 
@@ -13,15 +26,19 @@ z = cat[2]
 t = cat[3]
 n_gal = len(m_ref)
 
-#Loading filters & computing in_r......
-filt_list = np.loadtxt(filt_folder + filt_list, dtype = 'string', unpack = True)
-filt_list = np.hstack(([ref_filt], filt_list))
-n_filt = len(filt_list)
+#Loading exposure times................
+t_exp = np.loadtxt(exp_t_file, unpack = True)
 
+#Loading filters....................... 
+filt_list = np.loadtxt(filt_folder + filt_list, dtype = 'string', unpack = True)
+n_filt = len(filt_list)
+filt_list = np.hstack(([ref_filt], filt_list))
+
+#Computing in_r.........................
 y_r = {}
 x = {}
-in_r = np.zeros(n_filt)
-for i in range(n_filt): 
+in_r = np.zeros(n_filt + 1)
+for i in range(n_filt + 1): 
 	filt = filt_list[i]
 	R = np.loadtxt(filt_folder + filt, unpack = True)
 
@@ -32,7 +49,19 @@ for i in range(n_filt):
 	y_r[filt] = sp.interpolate.interp1d(R[0], R[1])(x[filt]) 
 	in_r[i] = (y_r[filt] / x[filt]).sum() * dx
 
-#Loading templates.....................
+in_r0 = in_r[0]
+in_r = in_r[1:]
+
+#Computing in_sky.......................
+SKY = np.loadtxt(sky_file, unpack = True) 
+in_sky = np.zeros(n_filt + 1)
+for i in range(n_filt + 1):
+	y_sky = sp.interpolate.interp1d(SKY[0], SKY[1])(x[filt])
+	in_sky[i] = (y_sky * y_r[filt]).sum() * dx
+
+in_sky = in_sky[1:]
+
+#Loading seds...........................
 sed_list = np.loadtxt(sed_folder + sed_list, dtype = 'string', unpack = True)
 n_sed = len(sed_list) 
 dt = (t.max() - t.min()) / (n_sed - 1)
@@ -41,28 +70,45 @@ t_range = np.arange(t.min(), t.max() + dt, dt )
 s = {}
 for sed in sed_list: s[sed] = np.loadtxt(sed_folder + sed, unpack = True)
 
-#Generating magnitudes.................
 f_noiseless = open(f_noiseless_file, "w") 
+f_noisely = open(f_noisely_file, "w") 
+
+#Iterating over all the galaxies
 for n in range(n_gal):
 	f_noiseless.write("%d " % id[n])
+	f_noisely.write("%d " % id[n])
 
 	s_ind_high = np.searchsorted(t_range, t[n])
 	s_high = sed_list[s_ind_high]
 	s_low = sed_list[s_ind_high - 1]
 	coef_high = (t_range[s_ind_high] - t[n]) / dt 
 	coef_low = 1 - coef_high  
-
-	in_s = np.zeros(n_filt)
-	for i in range(n_filt):
+	
+	#Computing in_s................
+	in_s = np.zeros(n_filt + 1)
+	for i in range(n_filt + 1):
 		filt = filt_list[i]
 		y_s_low = sp.interpolate.interp1d(s[s_low][0] * (1 + z[n]), s[s_low][1])(x[filt]) 
 		y_s_high = sp.interpolate.interp1d(s[s_low][0] * (1 + z[n]), s[s_low][1])(x[filt]) 
 		y_s = coef_high * y_s_high + coef_low * y_s_low
 		in_s[i] = (y_s * y_r[filt] * x[filt]).sum() * dx
 		
-		m = m_ref[n] + 2.5 * (log10(in_s[0]) + log10(in_r[i]) - log10(in_r[0]) - log10(in_s[i])) 
+	in_s0 = in_s[0]
+	in_s = in_s[1:]
 
-		if( (i != 0) & (i != (n_filt - 1))): f_noiseless.write("%4.4f " % m)
+	#Generating magnitudes.........
+	for i in range(n_filt):
+		m = m_ref[n] + 2.5 * (log10(in_s0) + log10(in_r[i]) - log10(in_r0) - log10(in_s[i])) 
+				
+		err_m_obs = err_magnitude()
+		rn = np.random.normal()
+		m_obs = m + rn * err_m_obs	
+
+		f_noiseless.write("%4.4f " % m)
+		f_noisely.write("%4.4f %4.4f " % (m_obs, err_m_obs))
+
 	f_noiseless.write("%4.4f %2.2f\n" % (z[n], t[n]))
+	f_noisely.write("%4.4f %2.2f\n" % (z[n], t[n]))
 
 f_noiseless.close()
+f_noisely.close()
--- parameters.py
+++ parameters.py
@@ -1,7 +1,10 @@
+from numpy import pi
+
 #Parameters.............................
 incat_file = "mock.r255.PAU.a010.s12.110521_reduced"
 incat_file_extention = ".dat"
 f_noiseless_file = incat_file + "_noiseless.cat"
+f_noisely_file = incat_file + "_noisely.cat"
 
 sed_folder = "./CE_NEW/"
 sed_list = "CE_NEW_MOD.txt"
@@ -9,11 +12,24 @@ sed_list = "CE_NEW_MOD.txt"
 filt_folder = "./PAU_filt/"
 filt_list = "PAU_filt_list.txt"
 
-ref_filt = "r_SDSS.res" #Reference filter
+exp_t_file = "exp_times.txt"
+sky_file = "sky_spectrum.txt"
+ref_filt = "i_SDSS.res" #Reference filter
 
 index_id = 0 #ID of the galaxy in de catalog
-index_m = 5 #This is the reference magnitude 
+index_m = 53 #This is the reference magnitude 
 index_z = 1 #The true redshift
 index_t = 2 #Spectral type. The associated template
 
 dx = 0.1 #Wavelength resolution in the integrals
+
+#Detector parameters.....................
+scale = 0.27 #arcsec/pixel
+RN = 5.0 #Read-out noise of the CCDS (electrons)
+D_tel = 4.2 #Diametre of the telescop (meters)
+aperture = 2.0 #Assumed standard size of the galaxies (arcsec2)
+n_exp = 2.0 #Number of expositions
+
+tel_surface = pi * (D_tel / 2) * (D_tel / 2) #Area of the telescope's mirror 
+pix_size = scale * scale #Size of the pixels in arcsec^2 
+n_pix = aperture / pix_size #Numer of pixels inside the aperture 
