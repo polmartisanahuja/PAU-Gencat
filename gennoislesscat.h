26eee6ca4908ceeea7ece1d21251fc959f99a33f
Polstein <polstein@MacBook-Air-de-Pol.local>
2013-08-12 16:44:30 +0200

    Removing files.

--- gennoislesscat.h
+++
@@ -1,8 +0,0 @@
-//Gennoiseless parameters
-char cat_out_file[1000] = "mock.r255.PAU.a010.s12.110521_reduced_noiseless_c.dat";
-char cat_in_file[1000] = "mock.r255.PAU.a010.s12.110521_reduced.dat";
-char nom_filt_file[1000] = "./PAU_filt/PAU_filt_list.txt";
-char nom_sed_file[1000] = "./CE_NEW/CE_NEW_MOD.txt";
-char nom_filt_folder[1000] = "./PAU_filt/";
-char nom_sed_folder[1000] = "./CE_NEW/";
-char nom_filt_seed[1000] = "i_SDSS";
